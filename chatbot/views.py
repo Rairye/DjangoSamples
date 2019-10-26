decde207da75eaef243f06ececcbea8d27867a50
Rairye <37942271+Rairye@users.noreply.github.com>
2019-10-26 09:17:49 +0900

    Add files via upload

---
+++ chatbot/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
+from . import ChatBot
+import time
+import threading
+
+chat_history = []
+
+
+def home (request):
+	global postable
+	random_sent = ChatBot.get_random()
+	chat_history.append("<span style = \"font-style: italic\"> - Bot: " + random_sent + "</span>")
+	response = {"chat_history" : chat_history}
+	return render(request, 'chatbot_manager/bot.html', response)
+
+@ensure_csrf_cookie
+def postresponse(request):
+	global chat_history
+	input = request.POST.get('input')
+	chat_history.append("- User: " + input)
+	response = {"chat_history" : chat_history}
+	return render(request, 'chatbot_manager/bot.html', response)
+		
+@ensure_csrf_cookie
+def getresponse(request):
+	postresponse(request)
+	global postable
+	postable = True
+	input = request.POST.get('input')
+	global chat_history
+	chat_response = ChatBot.get_response(input)
+	chat_history.append("<span style = \"font-style: italic\"> - Bot: " + chat_response + "</span>")
+	response = {"chat_history" : chat_history}
+	return render(request, 'chatbot_manager/bot.html', response)
